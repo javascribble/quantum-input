bc8d805bf0de81daecd33130c2247625e74737ad
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-07-20 11:49:46 -0400

    add basic checkbox functionality

--- source/elements/check.js
+++ source/elements/check.js
@@ -1,14 +1,44 @@
-import { Component, template } from '../../references/quantum.js';
-import html from '../templates/element.js';
+import { Component, template, setAttribute } from '../../references/quantum.js';
+import html from '../templates/check.js';
 
 export class Check extends Component {
+    #input;
+
     constructor() {
         super();
+
+        this.#input = this.shadowRoot.querySelector('input');
     }
 
     static template = template(html);
 
-    static attributes = [];
+    static attributes = ['state', 'disabled', 'name', 'value'];
+
+    stateChangedCallback(value) {
+        switch (value) {
+            case 'checked':
+                this.#input.checked = value;
+                break;
+            case 'indeterminate':
+                this.#input.indeterminate = true;
+                break;
+            default:
+                this.#input.removeAttribute('checked');
+                break;
+        }
+    }
+
+    disabledChangedCallback(value) {
+        setAttribute(this.#input, 'disabled', value);
+    }
+
+    nameChangedCallback(value) {
+        setAttribute(this.#input, 'name', value);
+    }
+
+    valueChangedCallback(value) {
+        setAttribute(this.#input, 'value', value);
+    }
 }
 
 customElements.define('quantum-check', Check);
--- source/templates/check.js
+++ source/templates/check.js
@@ -1,5 +1,9 @@
 export default `
 <style>
-
+    input {
+        transform: var(--transform);
+    }
 </style>
+<input id="checkbox" type="checkbox" />
+<label for="checkbox"><slot></slot></label>
 `;
